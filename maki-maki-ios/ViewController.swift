051932e6903f7e7b13595556e3c729bec83f45d0
soska <user@MacBook-Pro-user-3.local>
2023-05-16 09:37:29 +0600

    Initial commit

---
+++ maki-maki-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  maki-maki-ios
+//
+//  Created by soska on 16.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
