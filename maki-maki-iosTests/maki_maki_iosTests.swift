0312ab61ef0b057fe35ff67bdc0cd2db1f6b4f92
Galymzhan <galymzhan.aitkaliyev@gmail.com>
2023-05-24 12:57:45 +0600

    clean up comment in setUpWithError()

--- maki-maki-iosTests/maki_maki_iosTests.swift
+++ maki-maki-iosTests/maki_maki_iosTests.swift
@@ -10,7 +10,6 @@ import XCTest
 final class maki_maki_iosTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
