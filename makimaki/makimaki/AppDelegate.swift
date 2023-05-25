f6ff1a7d3e5f069a879f1fcf0bb4ccbba0045f51
darkhankktl <83265049+darkhankktl@users.noreply.github.com>
2023-05-25 21:29:07 +0600

    udalil probely

--- makimaki/makimaki/AppDelegate.swift
+++ makimaki/makimaki/AppDelegate.swift
@@ -10,22 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
-
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
        
         return UISceneConfiguration(name: "Standart Configuration", sessionRole: connectingSceneSession.role)
     }
-
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
+      
     }
 
 
