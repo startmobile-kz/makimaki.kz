ee89c6ca195eb3d227db157fca8cec7b3eb1b394
Islam <aitemr16@gmail.com>
2023-05-27 14:50:59 +0600

    [MAKI-3] удалил файл

--- maki-maki-ios/ViewController.swift
+++ maki-maki-ios/ViewController.swift
@@ -1,65 +1,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    func testFunc(){
-        print("Ravil")
-    }
-    
-    func testFunc(){
-        // решил, что функция должна быть в классе, поэтому удалил верхние коментарии. Или нужно было вставить эту функцию на 5 строку ничего не удаляя?")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello")
     }
-    
-    func ravilFunc(){
-        
-    }
-    
-    func viewControllerCom() {
-        // add some func
-    }
-    
-    
-    func newFuntionToMakeAPullRequest() {
-        
-        print("This is a dummy function which does nothing")
-        
-        
-    }
-    
-    func Blablabla() {
-        
-        // blablabla
-        
-    }
-    
-    func setupview() {
-        
-        view.backgroundColor = .systemBlue
-        
-    }
-    
-    func setupHierarchy() {
-        
-        let newView = UIView()
-        view.addSubview(newView)
-        
-    }
-    
-    func layout() {
-        //Function for creating layout
-        
-        //Also practice with comments submission
-    }
-
-    // Jasudakjhwckjwecnjndslknk
-    
-    
-    // Jadneiwhdowndjod
-
 }
-
