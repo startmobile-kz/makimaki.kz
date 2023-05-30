15376fda9bfea64a910db05838b6b49448fc9b6c
Ravil <star.toni@mail.ru>
2023-05-30 22:25:10 +0600

    Верстка 0.1

--- maki-maki-ios/EditingProfile/ProfileEditingViewController.swift
+++ maki-maki-ios/EditingProfile/ProfileEditingViewController.swift
@@ -10,12 +10,103 @@ import SnapKit
 
 class ProfileEditingViewController: UIViewController {
 
+    // MARK: - UI
+    
+    lazy var profileLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Profile"
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+
+        return label
+    }()
+    
+    lazy var profileImageView: UIImageView = {
+        let imageView = "man.png"
+        let image = UIImage(named: imageView)
+        let profileImageView = UIImageView(image: image!)
+        return profileImageView
+    }()
+    
+    lazy var nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Name"
+        return textField
+    }()
+    
+    lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Email"
+        return textField
+    }()
+    
+    lazy var phoneTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Phone"
+        return textField
+    }()
+    
+    lazy var saveButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("SAVE", for: .normal)
+        button.backgroundColor = .yellow
+        button.tintColor = .black
+        button.layer.cornerRadius = 14
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        setupViews()
+        setupContraints()
     }
     
+    // MARK: - Setup Views
     
+    private func setupViews() {
+        view.backgroundColor = .white
+        view.addSubview(profileLabel)
+        view.addSubview(profileImageView)
+        view.addSubview(nameTextField)
+        view.addSubview(emailTextField)
+        view.addSubview(phoneTextField)
+        view.addSubview(saveButton)
+    }
+    
+    private func setupContraints() {
+        profileLabel.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(156)
+            make.top.equalToSuperview().offset(58)
+        }
+        profileImageView.snp.makeConstraints { make in
+            make.top.equalTo(profileLabel.snp.bottom).offset(40)
+            make.left.equalToSuperview().inset(119)
+            make.right.equalToSuperview().inset(126)
+        }
+        nameTextField.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.bottom).offset(66)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+        }
+        emailTextField.snp.makeConstraints { make in
+            make.top.equalTo(nameTextField.snp.bottom).offset(66)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+        }
+        phoneTextField.snp.makeConstraints { make in
+            make.top.equalTo(emailTextField.snp.bottom).offset(66)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+        }
+        saveButton.snp.makeConstraints { make in
+            make.top.equalTo(phoneTextField.snp.bottom).offset(44.5)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.height.equalTo(53)
+        }
+    }
     
 }
