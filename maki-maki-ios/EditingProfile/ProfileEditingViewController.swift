7c91826fc604d47dd65ea1b69d8ac37d39943110
Ravil <star.toni@mail.ru>
2023-05-30 20:54:39 +0600

    Переименовал папку

---
+++ maki-maki-ios/EditingProfile/ProfileEditingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileEditingViewController.swift
+//  maki-maki-ios
+//
+//  Created by Ravil on 30.05.2023.
+//
+
+import UIKit
+import SnapKit
+
+class ProfileEditingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    
+}
