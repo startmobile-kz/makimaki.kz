efd21d8bfc36bcf3f6570e9a8e60d70ce18f3d5b
ZhenisTakhmina <zhenistakhmina@gmail.com>
2023-05-31 23:19:15 +0600

    [MAKI-22] Применила к верстке fonts and colors

--- maki-maki-ios/Registration/UITextFieldExtension.swift
+++ maki-maki-ios/Registration/UITextFieldExtension.swift
@@ -10,11 +10,10 @@ import UIKit
 
 // MARK: - Extension for border bottom line
 extension UITextField {
-    
    func addBottomBorder(){
        let bottomLine = CALayer()
        bottomLine.frame = CGRect(x: 0, y: self.frame.size.height - 1, width: self.frame.size.width, height: 0.5)
-       bottomLine.backgroundColor = UIColor(red: 224/256, green: 224/256, blue: 224/256, alpha: 1).cgColor
+       bottomLine.backgroundColor = AppColor.border.uiColor.cgColor
        borderStyle = .none
        layer.addSublayer(bottomLine)
    }
--- maki-maki-ios/Registration/WelcomePageViewController.swift
+++ maki-maki-ios/Registration/WelcomePageViewController.swift
@@ -9,54 +9,54 @@ import UIKit
 import SnapKit
 
 final class WelcomePageViewController: UIViewController {
-    
+
     // MARK: - UI elements design
    private lazy var welcomeLabel: UILabel = {
         let label = UILabel()
         label.text = "Welcome!"
-        label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        label.font = AppFont.bold.s32()
+       label.textColor = AppColor.heading.uiColor
         return label
-       //error
     }()
-    
+
     private lazy var subtitleLabel: UILabel = {
         let label = UILabel()
         label.text = "Sign In or Create Account to quickly manage orders"
-        label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
-        label.textColor = UIColor(red: 94/255, green: 94/255, blue: 94/255, alpha: 1)
+        label.font = AppFont.reqular.s15()
+        label.textColor = AppColor.paragraph.uiColor
         label.numberOfLines = 0
         label.lineBreakMode = .byWordWrapping
         return label
     }()
-    
+
     private lazy var phoneNumberTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "+7 7082020155"
         textField.borderStyle = .none
         return textField
     }()
-    
+
     private lazy var continueButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("CONTINUE", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.layer.cornerRadius = 14
-        button.backgroundColor = UIColor(red: 255/255, green: 204/255, blue: 102/255, alpha: 1)
+        button.backgroundColor = AppColor.accent.uiColor
        return button
     }()
-    
+
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpViews()
         setUpConstraints()
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         setUpViews()
     }
-    
+
     // MARK: - Setup Views
     private func setUpViews() {
         view.backgroundColor = .white
@@ -74,21 +74,20 @@ final class WelcomePageViewController: UIViewController {
             make.top.equalToSuperview().offset(93)
             make.height.equalTo(40)
         }
-        
+
         subtitleLabel.snp.makeConstraints { make in
             make.leading.equalToSuperview().offset(16)
             make.trailing.equalToSuperview().offset(-16)
             make.top.equalTo(welcomeLabel.snp.bottom).offset(13)
-            make.height.equalTo(36)
         }
-        
+
         phoneNumberTextField.snp.makeConstraints { make in
             make.top.equalTo(subtitleLabel.snp.bottom).offset(40)
             make.leading.equalToSuperview().offset(16)
             make.trailing.equalToSuperview().offset(-16)
             make.height.equalTo(60)
         }
-        
+
         continueButton.snp.makeConstraints { make in
             make.top.equalTo(phoneNumberTextField.snp.bottom).offset(32)
             make.leading.equalToSuperview().offset(16)
@@ -97,4 +96,3 @@ final class WelcomePageViewController: UIViewController {
         }
     }
 }
- 
