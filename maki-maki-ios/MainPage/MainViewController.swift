d66a00e46a6ec0313dc4c6470ca56a69b81e86b5
Damir Aliyev <damiraliyevcs@gmail.com>
2023-05-31 21:47:14 +0600

    [MAKI-6] Добавил главный вью контроллер.

---
+++ maki-maki-ios/MainPage/MainViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MainViewController.swift
+//  maki-maki-ios
+//
+//  Created by Damir Aliyev on 31.05.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    //MARK: -UI
+    
+    //MARK: -Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        
+    }
+    
+    private func setupConstraints() {
+        
+    }
+}
