cb0ba38f88ebbb09b58790271b205bf395bf76d3
Damir Aliyev <damiraliyevcs@gmail.com>
2023-05-31 22:39:25 +0600

    [MAKI-6] Добавил вьюшки для адресной информации.

--- maki-maki-ios/MainPage/MainViewController.swift
+++ maki-maki-ios/MainPage/MainViewController.swift
@@ -6,10 +6,44 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController {
     
     //MARK: -UI
+//    private lazy var deliveryStack: UIStackView = {
+//        let stack = UIStackView()
+//        stack.axis = .vertical
+//        stack.spacing = 5
+//
+//        return stack
+//    }()
+    
+    private lazy var deliverToLabel: UILabel = {
+        let label = UILabel()
+        label.text = "DELIVER TO"
+        label.font = AppFont.semibold.s15()
+        label.textColor = AppColor.darkOrange.uiColor
+        
+        return label
+    }()
+    
+    private lazy var addressLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Navoi 37"
+        label.font = AppFont.reqular.s15()
+        label.textColor = AppColor.heading.uiColor
+        
+        return label
+    }()
+    
+    private lazy var chevronButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "chevron.down"), for: .normal)
+        button.tintColor = AppColor.paragraph.uiColor
+    
+        return button
+    }()
     
     //MARK: -Lifecycle
     override func viewDidLoad() {
@@ -19,10 +53,29 @@ class MainViewController: UIViewController {
     }
     
     private func setupViews() {
-        
+        view.backgroundColor = AppColor.background.uiColor
+        edgesForExtendedLayout = []
+        let subviews = [deliverToLabel,addressLabel ,chevronButton]
+        view.addSubviews(subviews)
+       
     }
     
     private func setupConstraints() {
+        deliverToLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(18)
+            make.leading.equalTo(view.snp.leading).offset(16)
+        }
         
+        addressLabel.snp.makeConstraints { make in
+            make.top.equalTo(deliverToLabel.snp.bottom).offset(5)
+            make.leading.equalTo(view.snp.leading).offset(16)
+        }
+        
+        chevronButton.snp.makeConstraints { make in
+            make.centerY.equalTo(addressLabel.snp.centerY)
+            make.leading.equalTo(addressLabel.snp.trailing).offset(7.4)
+            make.width.equalTo(11.4)
+            make.height.equalTo(6)
+        }
     }
 }
