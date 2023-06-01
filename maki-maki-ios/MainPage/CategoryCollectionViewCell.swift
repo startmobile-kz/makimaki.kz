b968414c7a89a58b7935cf0b53481592dd902c42
alibekt1012 <alibekt1012@gmail.com>
2023-06-01 21:38:43 +0600

    commit for Damir with changes

---
+++ maki-maki-ios/MainPage/CategoryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCollectionViewCell.swift
+//  maki-maki-ios
+//
+//  Created by Almat Alibekov on 01.06.2023.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ maki-maki-ios/MainPage/PromoBannerCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PromoBannerCollectionViewCell.swift
+//  maki-maki-ios
+//
+//  Created by Almat Alibekov on 01.06.2023.
+//
+
+import UIKit
+
+class PromoBannerCollectionViewCell: UICollectionViewCell {
+    
+}
