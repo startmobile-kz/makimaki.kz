d2c2377d03146887cbf417a46bdb998c0c23a54d
daukesnurmukhan <daukesnurmukhan@gmail.com>
2023-06-04 22:37:55 +0500

    [MAKI-26] создал AccountListController

---
+++ maki-maki-ios/AccountList/AccountListController.swift
@@ -0,0 +1,29 @@
+//
+//  AccountListController.swift
+//  maki-maki-ios
+//
+//  Created by Нұрмұхан Дәукес on 04.06.2023.
+//
+
+import UIKit
+
+class AccountListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
