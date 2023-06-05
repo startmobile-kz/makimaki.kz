b493026512d1c9fe107931892ebbe42252cf51da
Islam <aitemr16@gmail.com>
2023-06-05 13:24:04 +0600

    fix mark

--- maki-maki-ios/Tabbar/MainTabbarController.swift
+++ maki-maki-ios/Tabbar/MainTabbarController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class MainTabbarController: UITabBarController {
 
-    /// MARK: - Lifecycle
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
