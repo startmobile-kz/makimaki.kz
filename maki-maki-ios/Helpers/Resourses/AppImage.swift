e0ba5c985b7028825cfb0442fa2dc860ad61aa96
ZhenisTakhmina <zhenistakhmina@gmail.com>
2023-06-05 14:21:48 +0600

    [MAKI-12]add icons and change file name to "check"

--- maki-maki-ios/Helpers/Resourses/AppImage.swift
+++ maki-maki-ios/Helpers/Resourses/AppImage.swift
@@ -34,6 +34,6 @@ enum AppImage: String, AppImageProtocol {
     case chevronDown = "chevron.down"
     case house = "house"
     case house_fill = "house.fill"
-    case check = "Check"
+    case check = "check"
     case eye = "eye"
 }
