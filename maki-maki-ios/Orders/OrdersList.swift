935c54849d490711b97e04222c4f8d29e3848a26
Galymzhan <galymzhan.aitkaliyev@gmail.com>
2023-06-07 01:08:31 +0600

    [MAKI-38] - Добавил модель OrdersList

---
+++ maki-maki-ios/Orders/OrdersList.swift
@@ -0,0 +1,14 @@
+//
+//  OrderList.swift
+//  maki-maki-ios
+//
+//  Created by siberianarg on 06.06.2023.
+//
+
+import Foundation
+
+struct OrdersList {
+//    let count: Int  необходимо сверстать в ячейке данный элемент
+    let positionName: String
+    let price: String
+}
