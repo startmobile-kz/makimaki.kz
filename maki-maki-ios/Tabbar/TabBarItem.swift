eebfdc816be1a773278ead6691b8934b80cca896
Damir Aliyev <damiraliyevcs@gmail.com>
2023-06-06 16:13:38 +0600

    [MAKI-51] Предварительно закончил конфигурацию TabBarItem.

--- maki-maki-ios/Tabbar/TabBarItem.swift
+++ maki-maki-ios/Tabbar/TabBarItem.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class TabBarItem: UIView {
     
@@ -16,7 +17,7 @@ class TabBarItem: UIView {
         return imageView
     }()
     
-    private lazy var dotImageView: UIImageView = {
+    private lazy var ellipseImageView: UIImageView = {
         let imageView = UIImageView(image: AppImage.ellipse.uiImage)
         
         return imageView
@@ -32,9 +33,44 @@ class TabBarItem: UIView {
         }
         self.image = tabImage
         super.init(frame: .zero)
+        setupViews()
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - SetupViews
+    private func setupViews() {
+        addSubviews([tabImageView, ellipseImageView])
+    }
+    
+    // MARK: - SetupConstraints
+    private func setupConstraints() {
+        tabImageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.height.equalTo(22)
+            make.height.equalTo(20)
+        }
+        
+        ellipseImageView.snp.makeConstraints { make in
+            make.height.equalTo(4)
+            make.width.equalTo(4)
+            make.bottom.equalTo(tabImageView.snp.bottom).offset(6)
+            make.centerX.equalTo(self.snp.center)
+        }
+    }
+    
+    func selectTab() {
+        ellipseImageView.alpha = 1
+        tabImageView.image = tabImageView.image?.withRenderingMode(.alwaysTemplate)
+        tabImageView.tintColor = AppColor.accent.uiColor
+     }
+     
+     func deselectTab() {
+         ellipseImageView.alpha = 0
+         tabImageView.image = tabImageView.image?.withRenderingMode(.alwaysTemplate)
+         tabImageView.tintColor = AppColor.paragraph.uiColor
+     }
 }
