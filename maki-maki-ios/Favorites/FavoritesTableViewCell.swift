784739c917ffd9fa362f9cf9bc8c622f1af9d4ec
AlexJust4lex <43409029+AlexJust4lex@users.noreply.github.com>
2023-06-07 13:09:51 +0600

    [MAKI-50] добавил UI и SetupViews

---
+++ maki-maki-ios/Favorites/FavoritesTableViewCell.swift
@@ -0,0 +1,112 @@
+//
+//  FavoritesTableViewCell.swift
+//  maki-maki-ios
+//
+//  Created by Александр Козлов on 07.06.2023.
+//
+
+import UIKit
+
+final class FavoritesTableViewCell: UITableViewCell {
+    
+    // MARK: - UI
+    
+    private lazy var favoritesImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "orderImage")
+        imageView.layer.cornerRadius = 16
+        return imageView
+    }()
+    
+    private lazy var favoritesNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Bellissimo Pizza"
+        label.font = AppFont.semibold.s18()
+        label.textColor = AppColor.heading.uiColor
+        return label
+    }()
+    
+    private lazy var favoritesDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Pizza, Sushi, Burgers, Salads, Desserts"
+        label.numberOfLines = 0
+        label.font = AppFont.reqular.s14()
+        label.textColor = AppColor.paragraph.uiColor
+        return label
+    }()
+    
+    private lazy var starImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "star")
+        return imageView
+    }()
+    
+    private lazy var ratingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "4.8"
+        label.font = AppFont.reqular.s14()
+        label.textColor = AppColor.heading.uiColor
+        return label
+    }()
+    
+    private lazy var firstDotSeparatorLabel: UILabel = {
+        let label = UILabel()
+        label.text = "•"
+        label.font = AppFont.reqular.s12()
+        label.textColor = AppColor.grey300.uiColor
+        return label
+    }()
+    
+    private lazy var deliveryTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "15-20 mins"
+        label.font = AppFont.reqular.s14()
+        label.textColor = AppColor.heading.uiColor
+        return label
+    }()
+    
+    private lazy var secondDotSeparatorLabel: UILabel = {
+        let label = UILabel()
+        label.text = "•"
+        label.font = AppFont.reqular.s12()
+        label.textColor = AppColor.grey300.uiColor
+        return label
+    }()
+    
+    private lazy var priceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "$$"
+        label.font = AppFont.reqular.s14()
+        label.textColor = AppColor.heading.uiColor
+        return label
+    }()
+    
+    // MARK: - Lifecyle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Views
+    
+    private func setupViews() {
+        [favoritesImageView, favoritesNameLabel, favoritesDescriptionLabel, starImageView, ratingLabel,
+         firstDotSeparatorLabel, deliveryTimeLabel, secondDotSeparatorLabel, priceLabel].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    // MARK: - Setup Constraints
+
+    private func setupConstraints() {
+        
+    }
+
+}
--- maki-maki-ios/Favorites/FavoritesViewController.swift
+++ maki-maki-ios/Favorites/FavoritesViewController.swift
@@ -7,10 +7,18 @@
 
 import UIKit
 
-class FavoritesViewController: UIViewController {
+final class FavoritesViewController: UIViewController {
     
     // MARK: - UI
     
+    private lazy var favoritesTableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(BasketTableViewCell.self, forCellReuseIdentifier: "favoritesCell")
+        tableView.rowHeight = 90
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
     
     // MARK: - Lifecycle
     
@@ -22,24 +30,36 @@ class FavoritesViewController: UIViewController {
         setupNavigationBar()
     }
     
-    
     // MARK: - Setup Navigation Bar
 
     private func setupNavigationBar() {
         self.navigationItem.title = "Favorites"
     }
     
-    
     // MARK: - Setup views
     
     private func setupViews() {
-        
+        view.addSubview(favoritesTableView)
     }
-    
     
     // MARK: - Setup constraints
 
     private func setupConstraints() {
         
+    }
+    
+}
+
+extension FavoritesViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "favoritesCell", for: indexPath)
+                as? FavoritesTableViewCell else {
+            fatalError("basketCell not found")
+        }
+        return cell
     }
 }
