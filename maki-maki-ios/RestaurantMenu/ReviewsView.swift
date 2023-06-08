11a1b95255f68010584f6af3379cb9b86ed6a0ad
Damir Aliyev <damiraliyevcs@gmail.com>
2023-06-08 13:20:03 +0600

    [MAKI-64] Добавил ReviewsView.

---
+++ maki-maki-ios/RestaurantMenu/ReviewsView.swift
@@ -0,0 +1,27 @@
+//
+//  ReviewsView.swift
+//  maki-maki-ios
+//
+//  Created by Damir Aliyev on 08.06.2023.
+//
+
+import UIKit
+import SnapKit
+
+class ReviewsView: UIView {
+    
+    // MARK: - Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        
+    }
+    
+    private func setupConstraints() {
+        
+    }
+}
