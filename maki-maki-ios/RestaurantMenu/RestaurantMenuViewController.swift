3a144cea2deb1032f556656466aaafafdec7e321
Damir Aliyev <damiraliyevcs@gmail.com>
2023-06-08 13:13:15 +0600

    [MAKI-64] Добавил контроллер и конфигурационные методы.

---
+++ maki-maki-ios/RestaurantMenu/RestaurantMenuViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RestaurantMenuViewController.swift
+//  maki-maki-ios
+//
+//  Created by Damir Aliyev on 08.06.2023.
+//
+
+import UIKit
+import SnapKit
+
+class RestaurantMenuViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        
+    }
+    
+    private func setupConstraints() {
+        
+    }
+}
