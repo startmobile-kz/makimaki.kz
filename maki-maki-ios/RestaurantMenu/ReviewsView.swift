ef92b4770e4ee8f5aec20db0aa527318041cc1d9
Damir Aliyev <damiraliyevcs@gmail.com>
2023-06-08 13:21:58 +0600

    [MAKI-64] Required init.

--- maki-maki-ios/RestaurantMenu/ReviewsView.swift
+++ maki-maki-ios/RestaurantMenu/ReviewsView.swift
@@ -10,11 +10,21 @@ import SnapKit
 
 class ReviewsView: UIView {
     
+    //MARK: - UI
+    lazy var starImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = AppImage.
+    }()
+    
     // MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
         setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     private func setupViews() {
