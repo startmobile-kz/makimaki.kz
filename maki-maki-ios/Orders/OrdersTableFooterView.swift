2e7c71b7a850b80c771101b41c5dfea6aa35ca80
Galymzhan <galymzhan.aitkaliyev@gmail.com>
2023-06-09 02:35:25 +0600

    [MAKI-38] - Clean up code

--- maki-maki-ios/Orders/OrdersTableFooterView.swift
+++ maki-maki-ios/Orders/OrdersTableFooterView.swift
@@ -9,7 +9,7 @@ import UIKit
 import SnapKit
 
 final class OrdersTableFooterView: UIView {
-
+    
     private lazy var reorderButton: UIButton = {
         let button = UIButton(type: .system)
         button.backgroundColor = AppColor.grey100.uiColor
@@ -27,20 +27,20 @@ final class OrdersTableFooterView: UIView {
         setupViews()
         setupConstraints()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     // MARK: - Setup Views
-
+    
     private func setupViews() {
         backgroundColor = AppColor.background.uiColor
         addSubview(reorderButton)
     }
-
+    
     // MARK: - Setup Constraints
-
+    
     private func setupConstraints() {
         reorderButton.snp.makeConstraints { make in
             make.leading.equalToSuperview().offset(16)
