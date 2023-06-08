716646022796ff32196dbb6be73fbd82178a5fea
alibekt1012 <alibekt1012@gmail.com>
2023-06-08 18:39:11 +0600

    commit for searhbar

---
+++ maki-maki-ios/Search/SearchResultTableViewCell.swift
@@ -0,0 +1,89 @@
+//
+//  SearchResultTableViewCell.swift
+//  maki-maki-ios
+//
+//  Created by Almat Alibekov on 06.06.2023.
+//
+
+import UIKit
+import SnapKit
+
+class SearchResultTableViewCell: UITableViewCell {
+    
+    // MARK: - UI
+    private lazy var productImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "cardImage_1")
+        imageView.layer.cornerRadius = 8
+        return imageView
+    }()
+    
+    private lazy var infoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    private lazy var productNameLabel: UILabel = {
+        
+       let label = UILabel()
+        label.text = "Medium Supremo Pizza"
+        label.font = AppFont.medium.s15()
+        label.textColor = AppColor.heading.uiColor
+        return label
+    }()
+    private lazy var priceLabel: UILabel = {
+        
+       let label = UILabel()
+        label.text = "14.40$"
+        label.font = AppFont.reqular.s14()
+        label.textColor = AppColor.paragraph.uiColor
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+        setupConstraints()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Views
+    private func setupViews() {
+        let subviews = [productNameLabel, priceLabel]
+        infoStackView.addSubviews(subviews)
+        contentView.addSubview(productImageView)
+        contentView.addSubview(infoStackView)
+    }
+    
+    // MARK: - Setup Constraints
+    private func setupConstraints() {
+        productImageView.snp.makeConstraints { make in
+            make.size.equalTo(50)
+            make.leading.equalTo(16)
+            make.top.equalTo(8)
+            make.bottom.equalTo(-8)
+        }
+        
+        infoStackView.snp.makeConstraints { make in
+            make.leading.equalTo(productImageView.snp.trailing).offset(16)
+            make.top.equalTo(11.5)
+            make.bottom.equalTo(-11.5)
+            make.trailing.equalTo(124)
+        }
+        
+        productNameLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+        }
+        
+        priceLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.top.equalTo(productNameLabel.snp.bottom).offset(8)
+        }
+    }
+}
---
+++ maki-maki-ios/Search/SearchV1ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  SearchV1ViewController.swift
+//  maki-maki-ios
+//
+//  Created by Almat Alibekov on 06.06.2023.
+//
+
+import UIKit
+
+final class SearchV1ViewController: UIViewController {
+    
+    // MARK: - UI
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.searchBarStyle = UISearchBar.Style.default
+        searchBar.placeholder = "Search for a dish"
+        searchBar.setImage(UIImage(named: "search_icon"), for: UISearchBar.Icon.search, state: .normal)
+        searchBar.setImage(UIImage(named: "clear_icon"), for: UISearchBar.Icon.clear, state: .normal)
+        searchBar.searchTextField.textColor = AppColor.heading.uiColor
+        searchBar.searchTextField.font = AppFont.reqular.s14()
+        searchBar.frame.size.height = 120
+        return searchBar
+    }()
+    private lazy var searchTableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(SearchResultTableViewCell.self, forCellReuseIdentifier: "searchResult_cell")
+        tableView.rowHeight = 66
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.separatorStyle = .none
+        return tableView
+        
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupContrains()
+    }
+    
+    // MARK: - Setup Views
+    private func setupViews() {
+        view.addSubview(searchBar)
+        view.addSubview(searchTableView)
+        view.backgroundColor = AppColor.background.uiColor
+    }
+
+    // MARK: - Setup Constraints
+    private func setupContrains() {
+        
+        searchBar.snp.makeConstraints { make in
+            make.leading.equalTo(16)
+            make.trailing.equalTo(-16)
+            make.top.equalTo(60)
+            make.height.equalTo(48)
+        }
+        
+        searchTableView.snp.makeConstraints { make in
+            
+            make.top.equalTo(searchBar.snp.bottom).offset(16)
+            make.leading.trailing.bottom.equalToSuperview()
+            make.height.equalTo(150)
+        }
+    }
+}
+
+extension SearchV1ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "searchResult_cell", for: indexPath)
+                as? SearchResultTableViewCell else {
+            fatalError("searchResultCell not found")
+        }
+        return cell
+    }
+    
+}
