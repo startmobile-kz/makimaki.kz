0ae5fe367ff62e0f2850c829a4d2199a60a71822
Damir Aliyev <damiraliyevcs@gmail.com>
2023-06-10 12:10:20 +0600

    [MAKI-69] Добавил NoOrdersView.

---
+++ maki-maki-ios/Orders/NoOrdersView.swift
@@ -0,0 +1,31 @@
+//
+//  NoOrdersView.swift
+//  maki-maki-ios
+//
+//  Created by Damir Aliyev on 10.06.2023.
+//
+
+import UIKit
+import SnapKit
+
+class NoOrdersView: UIView {
+    
+    // MARK: - Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        
+    }
+    
+    private func setupConstraints() {
+        
+    }
+}
