2ecbb62d81dee0f5229c798a1fd1951a29bd729c
alibekt1012 <alibekt1012@gmail.com>
2023-06-10 13:46:25 +0600

    [MAKI-54] Добавил RecentSearchesTableViewCell и SearchCategoriesTableViewCell

---
+++ maki-maki-ios/Search/RecentSearchesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecentSearchesTableViewCell.swift
+//  maki-maki-ios
+//
+//  Created by Almat Alibekov on 10.06.2023.
+//
+
+import UIKit
+
+class RecentSearchesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
