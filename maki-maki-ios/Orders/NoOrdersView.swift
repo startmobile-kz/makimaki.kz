40e8d8778a97cd5e48155bddea30513356584afc
Damir Aliyev <damiraliyevcs@gmail.com>
2023-06-10 12:11:27 +0600

    [MAKI-69] Добавил UI компоненты который сделал Галымжан.

--- maki-maki-ios/Orders/NoOrdersView.swift
+++ maki-maki-ios/Orders/NoOrdersView.swift
@@ -10,6 +10,33 @@ import SnapKit
 
 class NoOrdersView: UIView {
     
+    // MARK: - UI
+    
+    private lazy var noOrdersImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = AppImage.no_orders.uiImage
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private lazy var noOrderslabel: UILabel = {
+        let label = UILabel()
+        label.text = "No Orders Yet!"
+        label.textAlignment = .center
+        label.textColor = AppColor.heading.uiColor
+        label.font = AppFont.bold.s24()
+        return label
+    }()
+    
+    private lazy var proposalLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Discover and place your first order"
+        label.textAlignment = .center
+        label.textColor = AppColor.paragraph.uiColor
+        label.font = AppFont.reqular.s14()
+        return label
+    }()
+    
     // MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
