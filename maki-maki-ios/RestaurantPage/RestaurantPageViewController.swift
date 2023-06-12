a30f06051eace6c3dd24ab8ff2945490f0f6afa1
Damir Aliyev <damiraliyevcs@gmail.com>
2023-06-12 10:09:22 +0600

    [MAKI-85] Добавил RestaurantPageViewController.

---
+++ maki-maki-ios/RestaurantPage/RestaurantPageViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RestaurantPageViewController.swift
+//  maki-maki-ios
+//
+//  Created by Damir Aliyev on 12.06.2023.
+//
+
+import UIKit
+import SnapKit
+
+class RestaurantPageViewController: UIViewController {
+    
+    // MARK: - UI
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+    }
+    
+    // MARK: - SetupViews
+    private func setupViews() {
+        
+    }
+    
+    // MARK: - SetupConstraints
+    private func setupConstraints() {
+        
+    }
+}
