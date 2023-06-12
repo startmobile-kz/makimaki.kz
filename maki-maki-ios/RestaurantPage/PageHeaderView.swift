11d7c571cdeba4e0c333b228f2ab23531c34fb4d
Damir Aliyev <damiraliyevcs@gmail.com>
2023-06-12 10:44:57 +0600

    [MAKI-85] Добавил PageHeaderView.

--- maki-maki-ios/RestaurantPage/PageHeaderView.swift
+++ maki-maki-ios/RestaurantPage/PageHeaderView.swift
@@ -13,5 +13,21 @@ class PageHeaderView: UICollectionReusableView {
     // MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - SetupViews
+    private func setupViews() {
+        
+    }
+    
+    // MARK: - SetupConstraints
+    private func setupConstraints() {
+        
     }
 }
