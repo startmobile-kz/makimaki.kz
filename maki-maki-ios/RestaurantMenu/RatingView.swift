7d6d837ced6c268539571cf7998398359e8afdd6
Damir Aliyev <damiraliyevcs@gmail.com>
2023-06-13 13:23:40 +0600

    [MAKI-90] Добавил нужные UI компоненты в RatingView.

--- maki-maki-ios/RestaurantMenu/RatingView.swift
+++ maki-maki-ios/RestaurantMenu/RatingView.swift
@@ -11,6 +11,33 @@ import SnapKit
 class RatingView: UIView {
     
     // MARK: - UI
+    private lazy var starImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = AppImage.star.uiImage
+        return imageView
+    }()
+    
+    private lazy var ratingLabel: UILabel = {
+        let label = UILabel()
+        label.font = AppFont.reqular.s14()
+        label.textColor = AppColor.heading.uiColor
+        label.text = "4.8"
+        return label
+    }()
+    
+    private lazy var dotImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = AppImage.ellipse.uiImage
+        return imageView
+    }()
+    
+    private lazy var reviewsLabel: UILabel = {
+        let label = UILabel()
+        label.font = AppFont.reqular.s14()
+        label.textColor = AppColor.heading.uiColor
+        label.text = "500 Reviews"
+        return label
+    }()
     
     // MARK: - Lifecycle
     override init(frame: CGRect) {
