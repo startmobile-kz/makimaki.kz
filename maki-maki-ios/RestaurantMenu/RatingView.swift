a22021c0ab051ad014aa175a013dc437f247fdbb
Damir Aliyev <damiraliyevcs@gmail.com>
2023-06-13 13:08:28 +0600

    [MAKI-90] Создал RatingView.

---
+++ maki-maki-ios/RestaurantMenu/RatingView.swift
@@ -0,0 +1,35 @@
+//
+//  RatingView.swift
+//  maki-maki-ios
+//
+//  Created by Damir Aliyev on 13.06.2023.
+//
+
+import UIKit
+import SnapKit
+
+class RatingView: UIView {
+    
+    // MARK: - UI
+    
+    // MARK: - Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - SetupViews
+    private func setupViews() {
+        
+    }
+    
+    // MARK: - SetupConstraints
+    private func setupConstraints() {
+        
+    }
+}
