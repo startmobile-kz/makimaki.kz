1c6f82ccf2d200388bc3e0c10b9750e92e8a8e3b
Damir Aliyev <damiraliyevcs@gmail.com>
2023-06-13 13:04:12 +0600

    [MAKI-90] Создал RestaurantHeaderView.

---
+++ maki-maki-ios/RestaurantMenu/RestaurantHeaderView.swift
@@ -0,0 +1,35 @@
+//
+//  RestaurantHeaderView.swift
+//  maki-maki-ios
+//
+//  Created by Damir Aliyev on 13.06.2023.
+//
+
+import UIKit
+import SnapKit
+
+class RestaurantHeaderView: UICollectionReusableView {
+    
+    // MARK: - UI
+    
+    // MARK: - Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - SetupViews
+    private func setupViews() {
+        
+    }
+    
+    // MARK: - SetupConstraints
+    private func setupConstraints() {
+        
+    }
+}
