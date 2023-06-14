facdabc8e01d3eb4c673ef14ced21bb8bec3b77d
Damir Aliyev <damiraliyevcs@gmail.com>
2023-06-14 14:46:20 +0600

    [MAKI-90] Добавил текст в RatingView через AttributedString.

--- maki-maki-ios/RestaurantMenu/RatingView.swift
+++ maki-maki-ios/RestaurantMenu/RatingView.swift
@@ -40,6 +40,7 @@ final class RatingView: UIView {
     private func setupViews() {
         backgroundColor = AppColor.grey100.uiColor
         layer.cornerRadius = 14
+        ratingReviewsLabel.attributedText = makeAttributedString(rating: 4.8, reviews: 420)
         addSubviews([starImageView, ratingReviewsLabel])
     }
     
@@ -54,6 +55,20 @@ final class RatingView: UIView {
             make.centerY.equalToSuperview()
             make.leading.equalTo(starImageView.snp.trailing).offset(5.5)
         }
+    }
+    
+    private func makeAttributedString(rating: Float, reviews: Int) -> NSMutableAttributedString {
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: AppFont.reqular.s14(),
+            .foregroundColor: AppColor.heading.uiColor
+        ]
+        
+        let rootString = NSMutableAttributedString(string: String(rating), attributes: attributes)
+        let dotString = NSAttributedString(string: "  •  ", attributes: attributes)
+        let reviewsString = NSAttributedString(string: "\(reviews) Reviews")
+        rootString.append(dotString)
+        rootString.append(reviewsString)
+        return rootString
     }
     
     override var intrinsicContentSize: CGSize {
