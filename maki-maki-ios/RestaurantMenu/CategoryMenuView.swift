0b17362c8b828fd224916dfa1b164dd5d1b2034e
ZhenisTakhmina <zhenistakhmina@gmail.com>
2023-06-15 01:30:07 +0600

    [MAKI-92] Создала отдельную вьюшку для category menu

---
+++ maki-maki-ios/RestaurantMenu/CategoryMenuView.swift
@@ -0,0 +1,36 @@
+//
+//  CategoryMenuView.swift
+//  maki-maki-ios
+//
+//  Created by Tami on 15.06.2023.
+//
+
+import UIKit
+import SnapKit
+
+final class CategoryMenuView: UIView {
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - SetupViews
+    
+    private func setupViews() {
+    }
+    
+    // MARK: - SetupConstraints
+    
+    private func setupConstraints() {
+    }
+}
