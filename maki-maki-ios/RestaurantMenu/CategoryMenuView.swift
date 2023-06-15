1531f80ebe639faf1d4dc528419db174acc7c682
ZhenisTakhmina <zhenistakhmina@gmail.com>
2023-06-16 00:25:15 +0600

    [MAKI-92] Создала отдельную вьюшку для категорий

--- maki-maki-ios/RestaurantMenu/CategoryMenuView.swift
+++ maki-maki-ios/RestaurantMenu/CategoryMenuView.swift
@@ -10,23 +10,55 @@ import SnapKit
 
 final class CategoryMenuView: UIView {
     
+    // MARK: - State
+    
+    private var listCategory: [CategoryItem] = [
+        CategoryItem(title: "Most Popular", id: .mostPopular),
+        CategoryItem(title: "Pizza", id: .pizza),
+        CategoryItem(title: "Sushi", id: .sushi),
+        CategoryItem(title: "Rolls", id: .rolls),
+        CategoryItem(title: "Burgers", id: .burgers),
+        CategoryItem(title: "Breakfast", id: .breakfast),
+        CategoryItem(title: "Sandwichs", id: .sandwichs),
+        CategoryItem(title: "Kebab", id: .kebab),
+        CategoryItem(title: "Salads", id: .salads),
+        CategoryItem(title: "French Fries", id: .frenchFries),
+        CategoryItem(title: "Cold Drinks", id: .coldDrinks)
+    ]
+    
     // MARK: - UI
     
-    private let categoryLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Most Popular"
-        label.textAlignment = .center
-        label.textColor = AppColor.heading.uiColor
-        label.font = AppFont.reqular.s14()
-        return label
+    private lazy var categoryCollectionView: UICollectionView = {
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: flowLayout())
+        collection.dataSource = self
+        collection.delegate = self
+        collection.register(
+            DishCategoryCollectionViewCell.self,
+            forCellWithReuseIdentifier: DishCategoryCollectionViewCell.reuseID)
+        return collection
     }()
+    
+    private func setupCollection() {
+        categoryCollectionView.showsHorizontalScrollIndicator = false
+        categoryCollectionView.bounces = false
+        categoryCollectionView.selectItem(at: [0,0], animated: true, scrollPosition: [])
+    }
+    
+    private func flowLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumInteritemSpacing = 4
+        return layout
+    }
     
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
         setupViews()
         setupConstraints()
+        setupCollection()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -34,25 +66,57 @@ final class CategoryMenuView: UIView {
     }
     
     override var intrinsicContentSize: CGSize {
-        return CGSize(width: 114, height: 40)
+        return CGSize(width: 0, height: 40)
     }
     
     // MARK: - SetupViews
     
     private func setupViews() {
-        backgroundColor = AppColor.accent.uiColor
-        layer.cornerRadius = 14
-        
-        addSubview(categoryLabel)
+        addSubview(categoryCollectionView)
     }
     
     // MARK: - SetupConstraints
     
     private func setupConstraints() {
-        
-        categoryLabel.snp.makeConstraints { make in
-            make.centerY.equalToSuperview()
-            make.centerX.equalToSuperview()
+        categoryCollectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
         }
+    }
+}
+
+extension CategoryMenuView: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        listCategory.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: DishCategoryCollectionViewCell.reuseID,
+            for: indexPath) as? DishCategoryCollectionViewCell else {
+            fatalError("Couldn't cast to DishCategoryCollectionViewCell")
+        }
+        cell.categoryLabel.text = listCategory[indexPath.item].title
+        return cell
+    }
+}
+
+extension CategoryMenuView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
+}
+
+extension CategoryMenuView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let padding: CGFloat = 28.0
+        let categoryFont = AppFont.reqular.s14()
+        let categoryAttribuites = [NSAttributedString.Key.font: categoryFont as Any]
+        let categoryWidth = listCategory[indexPath.item].title.size(
+            withAttributes: categoryAttribuites).width + padding
+        return CGSize(width: categoryWidth, height: collectionView.frame.height)
     }
 }
