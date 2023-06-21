d2ac1c6304129939391dd5b48f07b9fe63bbd223
ZhenisTakhmina <zhenistakhmina@gmail.com>
2023-06-21 15:12:08 +0600

    [MAKI-97] Удалила из AppImage ненужную картинку

--- maki-maki-ios/Helpers/Resourses/AppImage.swift
+++ maki-maki-ios/Helpers/Resourses/AppImage.swift
@@ -69,7 +69,6 @@ enum AppImage: String, AppImageProtocol {
     case clear_icon = "clear_icon"
     case clock = "clock"
     case star = "star"
-    case dish = "dish"
     case restaurant_top_image = "restaurant_top_image"
     case pizza_image = "pizza_image"
     case add_button = "add_button"
