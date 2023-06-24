55a2fa14b84e43d5095dc384bb98c657691061bb
Islam Temirbek <aitemr16@gmail.com>
2023-06-24 14:48:54 +0600

    Revert "[MAKI-62] - Реализовать главный экран ресторана (in process)"

--- maki-maki-ios/Dish/DishViewController.swift
+++ maki-maki-ios/Dish/DishViewController.swift
@@ -8,24 +8,7 @@
 import UIKit
 import SnapKit
 
-protocol DishViewControllerDelegate: AnyObject {
-    func addToBasket(dish: DishResponseModel)
-}
-
 final class DishViewController: UIViewController {
-    
-    // MARK: - State
-
-    var dish: DishResponseModel?
-    var count = 1 {
-        didSet {
-            orderPrice.text = "\((dish?.price ?? 0) * count) ₸"
-        }
-    }
-
-    // MARK: - Delegate
-    
-    weak var delegate: DishViewControllerDelegate?
     
     // MARK: - UI
     
@@ -39,6 +22,7 @@ final class DishViewController: UIViewController {
     
     private lazy var productNameLabel: UILabel = {
         let label = UILabel()
+        label.text = "Medium Supremo Pizza"
         label.textAlignment = .left
         label.textColor = AppColor.heading.uiColor
         label.font = AppFont.bold.s24()
@@ -47,6 +31,7 @@ final class DishViewController: UIViewController {
     
     private lazy var descriptionLabel: UILabel = {
         let label = UILabel()
+        label.text = "Pepperoni, mushrooms, salami, onions, bell peppers, sausage, and olives."
         label.textAlignment = .left
         label.numberOfLines = 0
         label.textColor = AppColor.paragraph.uiColor
@@ -55,7 +40,7 @@ final class DishViewController: UIViewController {
     }()
     
     private lazy var stepperView: StepperView = {
-        let stepper = StepperView(count: dish?.count ?? 1)
+        let stepper = StepperView()
         stepper.contentMode = .scaleAspectFill
         stepper.clipsToBounds = true
         stepper.layer.cornerRadius = 14
@@ -73,16 +58,7 @@ final class DishViewController: UIViewController {
         button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
         button.tintColor = AppColor.heading.uiColor
         button.titleLabel?.font = AppFont.medium.s15()
-        button.addTarget(self, action: #selector(addButtonDidPressed), for: .touchUpInside)
         return button
-    }()
-    
-    private lazy var orderPrice: UILabel = {
-        let label = UILabel()
-        label.textAlignment = .center
-        label.font = AppFont.medium.s15()
-        label.textColor = AppColor.heading.uiColor
-        return label
     }()
     
     // MARK: - Lifecycle
@@ -92,7 +68,6 @@ final class DishViewController: UIViewController {
         
         setupViews()
         setupConstraints()
-        setupData()
     }
     
     // MARK: - Setup Views
@@ -104,8 +79,7 @@ final class DishViewController: UIViewController {
             productNameLabel,
             descriptionLabel,
             stepperView,
-            addButton,
-            orderPrice
+            addButton
         ].forEach { view.addSubview($0) }
     }
     
@@ -116,7 +90,6 @@ final class DishViewController: UIViewController {
             make.top.equalToSuperview().offset(31)
             make.leading.equalToSuperview().offset(31)
             make.trailing.equalToSuperview().offset(-30)
-            make.height.equalTo(304)
         }
         
         productNameLabel.snp.makeConstraints { make in
@@ -144,43 +117,10 @@ final class DishViewController: UIViewController {
             make.trailing.equalToSuperview().offset(-16)
             make.height.equalTo(53)
         }
-        
-        orderPrice.snp.makeConstraints { make in
-            make.bottom.equalToSuperview().offset(-29)
-            make.trailing.equalToSuperview().offset(-32)
-            make.height.equalTo(53)
-        }
-        
     }
-    
-    // MARK: - Stepper Value Action
     
     @objc private func stepperChangedValueAction(sender: StepperView) {
-        self.count = sender.currentValue
-    }
-
-    // MARK: - Button Action
-    
-    @objc private func addButtonDidPressed() {
-        if let dish = dish {
-            dish.isSelected = true
-            dish.count = count
-            delegate?.addToBasket(dish: dish)
-            dismiss(animated: true)
-        }
-    }
-    
-    // MARK: - Setup Data
-
-    private func setupData() {
-        guard let dish = dish else {
-            return
-        }
-        
-        productNameLabel.text = dish.name
-        descriptionLabel.text = "\(dish.description)"
-        orderPrice.text = "\(dish.price) ₸"
-        let url = URL(string: dish.image ?? "")
-        dishImageView.kf.setImage(with: url)
+        print(sender)
+        print(sender.currentValue)
     }
 }
