36ee20c5902fb41320b0e5fe31d40c8dc6c8aa9a
Damir Aliyev <damiraliyevcs@gmail.com>
2023-06-30 13:00:57 +0600

    [MAKI-132] Создал SecondMainViewController.

---
+++ maki-maki-ios/NewMainPage/SecondMainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondMainViewController.swift
+//  maki-maki-ios
+//
+//  Created by Damir Aliyev on 30.06.2023.
+//
+
+import UIKit
+import SnapKit
+
+class SecondMainViewController: UIViewController {
+    
+}
