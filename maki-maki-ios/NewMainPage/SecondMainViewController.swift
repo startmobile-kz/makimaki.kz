9772557c9dbb5437223d2f44fe6f6331a281f4bc
ZhenisTakhmina <zhenistakhmina@gmail.com>
2023-07-04 15:18:32 +0600

    [MAKI-139] Удалила принт лишний

--- maki-maki-ios/NewMainPage/SecondMainViewController.swift
+++ maki-maki-ios/NewMainPage/SecondMainViewController.swift
@@ -531,7 +531,6 @@ extension SecondMainViewController: DishViewControllerDelegate {
         collectionView.reloadData()
          
         productsByCategoryMap.values.forEach { products in
-            print(products.description)
             selectedProducts.append(contentsOf: products.filter({ product in
                 return product.isSelected && !selectedProducts.contains(where: {$0.id == product.id})
             }))
