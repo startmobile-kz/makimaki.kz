5c32faa63f9efdbe6cd734adea53afbec090379d
Galymzhan <galymzhan.aitkaliyev@gmail.com>
2023-07-05 11:17:09 +0600

    [MAKI-136] - Move func autorizeWithAF to WelcomePageService

--- maki-maki-ios/Registration/Model/WelcomePageService.swift
+++ maki-maki-ios/Registration/Model/WelcomePageService.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import Alamofire
+
+class WelcomePageService {
+    
+    private func authorizeWithAlamofire(phoneNumber: String, deviceID: String) {
+        let = "https://app.makimaki.kz/api/v1/client/phone-confirmation/request"
+        
+        let parameters: [String: Any] = [
+            "uuid": deviceID,
+            "phone": phoneNumber
+        ]
+        
+        AF.request(urlString, method: .post, parameters: parameters, encoding: JSONEncoding.default)
+            .response { response in
+                if let error = response.error {
+                    print(error)
+                } else if let data = response.data {
+                    let jsonData = try? JSONSerialization.jsonObject(with: data, options: [])
+                    print(jsonData)
+                }
+            }
+    }
+}
