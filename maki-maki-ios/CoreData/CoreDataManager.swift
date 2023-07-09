0ecebe53e3113cc2c7e2a1995d9b32c52d7ea440
Damir Aliyev <damiraliyevcs@gmail.com>
2023-07-09 19:19:10 +0600

    [MAKI-152] Добавил синглтон CoreDataManager.

---
+++ maki-maki-ios/CoreData/CoreDataManager.swift
@@ -0,0 +1,21 @@
+//
+//  CoreDataManager.swift
+//  maki-maki-ios
+//
+//  Created by Damir Aliyev on 09.07.2023.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    // MARK: State
+    
+    let shared = CoreDataManager()
+    
+    // MARK: Lifecycle
+    
+    private init() {}
+
+}
