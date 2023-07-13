c24309d173d123f927ecc0e6eafc469215013eb9
ZhenisTakhmina <zhenistakhmina@gmail.com>
2023-07-13 17:52:14 +0600

    [MAKI-MVVM-NEW] логика вью модельки

--- maki-maki-ios/SelectLocation/ViewModel/AddressViewModel.swift
+++ maki-maki-ios/SelectLocation/ViewModel/AddressViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+class AddressViewModel {
+    
+    private let service = AddressService()
+    
+    var addresses: [Address] = [] {
+        didSet {
+            reloadDataCallback?()
+        }
+    }
+    
+    var reloadDataCallback: (() -> Void)?
+    
+    func saveAddress(with address: Address) {
+        let newAddress = Address(id: UUID(),
+                                 street: address.street,
+                                 latitude: address.latitude,
+                                 longitude: address.longitude,
+                                 house: address.house,
+                                 flat: address.flat,
+                                 type: address.type)
+        service.saveAddress(address: newAddress)
+    }
+    
+    func fetchAdresses() {
+        addresses = service.fetchAddresses()
+    }
+    
+    func deleteAddress(index: IndexPath) {
+        let address = addresses[index.row]
+        service.deleteAddress(address: address)
+        addresses.remove(at: index.row)
+    }
+}
